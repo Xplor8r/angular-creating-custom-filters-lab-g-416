57f26740c01d321df9ca65b3040483939e529df7
Michael Pieschel <sixtyhectares@yahoo.com>
2019-04-09 22:15:08 +0000

    Done.

---
+++ js/app/filters/City.js
@@ -0,0 +1,10 @@
+function City() {
+  return function(list, city) {
+    return list.filter(function(item) {
+      return item.location.city === city;
+    });
+  };
+}
+angular
+  .module('app')
+  .filter('city', City)
